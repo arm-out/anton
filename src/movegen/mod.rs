9c6a0e3b909fef1b444a5bacff64dd151306888b
arm-out <arminsuraj@gmail.com>
2026-05-15 03:35:54 +1000

    refactor

--- src/movegen/mod.rs
+++ src/movegen/mod.rs
@@ -1,14 +1,20 @@
-use crate::board::{bitboard::Bitboard, piece::Color, square::Square};
+use crate::{
+    board::{bitboard::Bitboard, piece::Color, square::Square},
+    movegen::directions::{
+        KING_SHIFTS, KNIGHT_SHIFTS, MoveShift, PAWN_SHIFT_BLACK, PAWN_SHIFT_WHITE,
+    },
+};
 
-mod attack;
 mod directions;
 
 pub struct MoveGenerator {
-    pub king_moves: [Bitboard; Square::COUNT],
-    pub knight_moves: [Bitboard; Square::COUNT],
-    pub pawn_attacks: [[Bitboard; Square::COUNT]; Color::COUNT],
-    pub rook_moves: Vec<Bitboard>,
-    pub bishop_moves: Vec<Bitboard>,
+    king_moves: [Bitboard; Square::COUNT],
+    knight_moves: [Bitboard; Square::COUNT],
+    pawn_attacks: [[Bitboard; Square::COUNT]; Color::COUNT],
+    rook_moves: Vec<Bitboard>,
+    bishop_moves: Vec<Bitboard>,
+    // rook_magics: [Magic; Square::COUNT],
+    // bishop_magics: [Magic; Square::COUNT],
 }
 
 impl MoveGenerator {
@@ -29,5 +35,49 @@ impl MoveGenerator {
         }
 
         movegen
+    }
+
+    fn init_pawn_attacks(&mut self, square: Square, color: Color) {
+        let mask = Bitboard::from_square(square);
+        let shifts = match color {
+            Color::White => PAWN_SHIFT_WHITE,
+            Color::Black => PAWN_SHIFT_BLACK,
+        };
+
+        for MoveShift { shift, exclude } in shifts {
+            let candidate = if color == Color::White {
+                (mask & !exclude) << shift
+            } else {
+                (mask & !exclude) >> -shift
+            };
+
+            self.pawn_attacks[color][square] |= candidate;
+        }
+    }
+
+    fn init_king_moves(&mut self, square: Square) {
+        let mask = Bitboard::from_square(square);
+
+        for MoveShift { shift, exclude } in KING_SHIFTS {
+            let candidate = if shift > 0 {
+                (mask & !exclude) << shift
+            } else {
+                (mask & !exclude) >> -shift
+            };
+            self.king_moves[square] |= candidate;
+        }
+    }
+
+    fn init_knight_moves(&mut self, square: Square) {
+        let mask = Bitboard::from_square(square);
+
+        for MoveShift { shift, exclude } in KNIGHT_SHIFTS {
+            let candidate = if shift > 0 {
+                (mask & !exclude) << shift
+            } else {
+                (mask & !exclude) >> -shift
+            };
+            self.knight_moves[square] |= candidate;
+        }
     }
 }
