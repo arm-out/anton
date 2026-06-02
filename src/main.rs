52a22d8518f28debdfdc21c10db70d1a7c62df33
arm-out <arminsuraj@gmail.com>
2026-06-02 16:01:04 +1000

    anton bench for ci

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
     if std::env::args().nth(1).as_deref() == Some("bench") {
-        anton::benchmark::run();
+        let ci: bool = if std::env::args().nth(2).as_deref() == Some("--ci") {
+            true
+        } else {
+            false
+        };
+        anton::benchmark::run(ci);
     } else {
         anton::uci::run();
     }
