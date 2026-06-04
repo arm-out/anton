2a8d1c95d5da7b461411f48004c1771ae869c60e
arm-out <arminsuraj@gmail.com>
2026-06-05 04:29:57 +1000

    update perft positions

--- benches/perft.rs
+++ benches/perft.rs
@@ -10,28 +10,34 @@ struct PerftPosition {
 
 const POSITIONS: &[PerftPosition] = &[
     PerftPosition {
-        name: "startpos_d5",
+        name: "startpos_d7",
         fen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-        depth: 5,
-        nodes: 4_865_609,
+        depth: 7,
+        nodes: 3_195_901_860,
     },
     PerftPosition {
-        name: "kiwipete_d4",
+        name: "kiwipete_d6",
         fen: "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
-        depth: 4,
-        nodes: 4_085_603,
+        depth: 6,
+        nodes: 8_031_647_685,
     },
     PerftPosition {
-        name: "position3_d5",
-        fen: "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1",
-        depth: 5,
-        nodes: 674_624,
+        name: "position3_d7",
+        fen: "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1 ",
+        depth: 7,
+        nodes: 178_633_661,
     },
     PerftPosition {
-        name: "position4_d4",
+        name: "position4_d7",
         fen: "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
-        depth: 4,
-        nodes: 422_333,
+        depth: 7,
+        nodes: 706_045_033,
+    },
+    PerftPosition {
+        name: "steven_d6",
+        fen: "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
+        depth: 6,
+        nodes: 6_923_051_137,
     },
 ];
 
